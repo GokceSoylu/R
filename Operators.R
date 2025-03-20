9889f76756723c46ae3cb613c59a369c050fd3a4
GokceSoylu <gokcesoylu24@gmail.com>
2025-03-20 16:30:43 +0300

    logical operator and

--- Operators.R
+++ Operators.R
@@ -7,3 +7,26 @@
 17 %% 3
 17 %/% 3 # tam bölme
 17 / 3 #float bölme
+
+
+# şöyle komik bir şey var 
+3 -> x
+x
+
+#comparasion(karşılaştırma) operatörleri C ile aynı 
+#  mantık operatorlerinin tamamı var  and olarak iki çeşit var 
+# && kısa yolu kullanma sadece ilk elemanları karşılaştırma, & tüm elemanları karşılaştırma
+a <- c(FALSE, FALSE, TRUE) #c fonksiyonu vektor oluşturmak için kullanılır
+b = c(FALSE, TRUE, TRUE)
+c = c(FALSE)
+
+# r1 = c && b # buraada hata alırsın çünkü sadece ilk elemanları kıyaslıyacağı için tek elemanlıları karşılaştırabilir
+
+r1 = c && b # eğer c tru olsa yine karşılaştırmayı kabul etmezdi
+r1 # kısaca && sadece bir elemanı karşılaştırabildiği için tek elemanlı verktörleri kıyaslar tekliye karşılık çokklu vektörü kıyaslıyamaz ancak tekli vektörüm fals eise çıktı her zaman false olacağı için hata vermez false çıktısını verir ancak tekli vektör true ise devamında BELİKİDE TRUE gelem ihtimaline karşı kıyaslıyamayacağı için kabul etmez hat verrir
+
+
+r2 = a & b
+r2
+
+
