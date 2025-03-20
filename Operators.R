05251c204d61e6d73ef79fa243ba7ba14a6a9b66
GokceSoylu <gokcesoylu24@gmail.com>
2025-03-20 15:37:10 +0300

    operators

---
+++ Operators.R
@@ -0,0 +1,9 @@
+
+#+(Addition), -(Subtraction), *(Multiplication), /(Division), ^(Exponent), 
+#%%(Modulus)(Remainder from division), %/%	(Integer Division)
+5 %% 2
+5 %/% 2
+
+17 %% 3
+17 %/% 3 # tam bölme
+17 / 3 #float bölme
