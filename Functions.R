684ef39bf662c75225213534456b73903d763a9e
GokceSoylu <gokcesoylu24@gmail.com>
2025-03-20 17:18:58 +0300

    basic function example

---
+++ Functions.R
@@ -0,0 +1,8 @@
+#fonksiyon olıuşturmak içn x = function() gibi bir yapı oluşturuyoruz yani değişkene fonsiyon atıyoruz bunu daha önce python ve js'de de gördük. pythondaki lambda yapısı js'deki de vektör fonksiyon yapısına benziyor.
+
+theF = function(n)
+    n + 12
+
+theF(12)
+# function tanımında culy brackets kullanabilirsin ancak olmasa da çalışır
+
