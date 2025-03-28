766de6c119a0b8fc10ae2910082c7c14dc0c997a
GokceSoylu <gokcesoylu24@gmail.com>
2025-03-28 11:46:59 +0300

    matemetiksel

--- Basic/math.R
+++ Basic/math.R
@@ -5,3 +5,28 @@ sqrt(16)
 abs(-12.3)
 ceiling(12.3)
 floor(12.3)
+
+
+print("slayttan denemeler")
+#sqrt(-9) In sqrt(-9) : NaNs produced
+
+exp(1)
+
+#log() fonksiyou ln tabamında x olarak tanımlanır yani lnx demektir
+#exp() ise e^x olarak tanımlanır
+#hmm eğer  ki ln değil tabanı benim belirleyeceğim bir logaritma olsun dersen log(x= , base=) fonksiyonunu kullanırsın
+#ayrıca sık kullanıldığı için log10() ve log2() fonksiyonları hazır olarak bulunru
+#özetle log()[ln demek] log(x=,base=) log2() log10() ve exp()[e^x] fonksiyonları hazır olarak mevcuttur
+
+print("değişik")
+
+#kombinasyon işlemi yapılabilir, 
+#faktoryel işlemi yapılabilir
+choose(9, 8)
+factorial(5)
+
+#yuvarlama içnde zaten floor()alta yuvarlama ceiling() üset yuvarlama.
+#ayrıca  roound(x, digits=) virgülden sonra istediğimiz kadar sayı bırakır 
+#signif(x, digits= ) soldan istediğimiz kadar rakam bırakır
+round(pi, 5)
+signif(12345.123456789, 6)
